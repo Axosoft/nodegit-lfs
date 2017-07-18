858e1d4edfe927a0589fe74a1cb74d6dbd90f1d3
Mohseen Mukaddam <mohseenmukaddam6@gmail.com>
2017-07-18 16:28:27 -0700

    Debugging Check and Apply callbacks [Does not work as intended]

--- src/applyCallback.js
+++ src/applyCallback.js
@@ -1,17 +1,26 @@
+/* eslint-disable consistent-return */
 import path from 'path';
 import { exec } from './utils/execHelpers';
 
 const apply = (to, from, source) => {
-  const realPath = path.join(source.repo().workdir(), source.path());
-  const command = `cat ${realPath} | git lfs clean`;
+  const mode = source.mode();
+  // const sourcePath = source.path();
+  /* Clean */
+  if (mode === 1) {
+    console.log('Clean Path: ', source.path());
+    const realPath = path.join(source.repo().workdir(), source.path());
+    const command = `cat ${realPath} | git lfs clean`;
 
-  return exec(command).then(({ stdout }) => {
-    const sha = new Buffer(stdout);
-    return to.set(sha, sha.length).then(() => 0);
-  }).catch((err) => {
-    console.log('Filter Error: ', err);
-    return 0;
-  });
+    return exec(command).then(({ stdout }) => {
+      const sha = new Buffer(stdout);
+      return to.set(sha, sha.length).then(() => 0);
+    }).catch((err) => {
+      console.log('\nFilter Error: ', err);
+      return 0;
+    });
+  } /* Smudge */ else if (mode === 0) {
+    return console.log('Smudge Path: ', source.path());
+  }
 };
 
 export { apply };
--- src/checkCallback.js
+++ src/checkCallback.js
@@ -1,6 +1,8 @@
-const check = () => {
-  console.log('Inside CHECK');
-  return 0;
+const check = (src, attr) => {
+  console.log('Inside CHECK: ', attr);
+  console.log('Source: ', src.path());
+  console.log('Source mode: ', src.mode());
+  return src.path().includes('.txt') ? 0 : -30;
 };
 
 export { check };
