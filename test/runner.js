103f3608e64c5966e4aea80e682c4260687f8186
Mohseen Mukaddam <mohseenmukaddam6@gmail.com>
2017-07-27 16:14:16 -0700

    commit fix

--- test/runner.js
+++ test/runner.js
@@ -34,7 +34,7 @@ before(function() {
 
 //eslint-disable-next-line
 beforeEach(function() {
-  this.timeout(4000);expect(result).to.be.a('number');
+  this.timeout(4000);
   return exec('git clean -xdf', { cwd: workdirPath })
     .then(() => exec('git checkout test', { cwd: workdirPath }))
     .then(() => exec('git reset --hard', { cwd: workdirPath }))
