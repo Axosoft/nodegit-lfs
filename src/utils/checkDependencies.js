a0f18a9f2165428298165cca46c4778dbe1a3a68
Carson Howard <carsonh@axosoft.com>
2017-07-27 12:05:09 -0700

    added some tests and cleanup

--- src/utils/checkDependencies.js
+++ src/utils/checkDependencies.js
@@ -1,86 +1,40 @@
 
 import fse from 'fs-extra';
 import path from 'path';
+import R from 'ramda';
+
+import {
+  regex as versionRegexes,
+  BAD_VERSION,
+  minimumVersions,
+} from '../constants';
 
 /**
- * @function generateNormalizedVersionNumber
+ * @function normalizeVersion
  * @param  Array<string> versionArray array of version number eg: ['1', '8', '3'] => 1.8.3
  * @return Number normalized version number
  */
-const generateNormalizedVersionNumber = (versionArray) => {
-  if (versionArray && versionArray.length > 0) {
-    let normalizedVersionNumber = 0;
-    const minorScale = 1000;
-    const majorScale = minorScale * 1000;
-    /* Major */
-    normalizedVersionNumber += parseInt(versionArray[0] * majorScale, 10);
-    /* Minor */
-    if (versionArray.length > 1) {
-      normalizedVersionNumber += parseInt(versionArray[1] * minorScale, 10);
-    }
-    /* Patch */
-    if (versionArray.length > 2) {
-      normalizedVersionNumber += parseInt(versionArray[2], 10);
-    }
-    return normalizedVersionNumber;
+const normalizeVersion = (versionArray) => {
+  if (!versionArray || versionArray.length === 0) {
+    return BAD_VERSION;
   }
-  return null;
+  return R.join('.', versionArray);
 };
 
-const isAtleastGitVersion = (actualVersion, minVersion) => {
-  const gitVersionRegex = /(?:git version\s+)?(\d+)(?:.(\d+))?(?:.(\d+))?.*/g;
-
-  let filteredActualVersion;
-  let filteredMinVersion;
-
-  try {
-    const minVersionMatch = gitVersionRegex.exec(minVersion);
-    const actualVersionMatch = gitVersionRegex.exec(actualVersion);
-    filteredActualVersion = actualVersionMatch.filter(match => !isNaN(parseInt(match, 10)));
-    filteredMinVersion = minVersionMatch.filter(match => !isNaN(parseInt(match, 10)));
-    // remove the full string match
-    filteredActualVersion.shift();
-    filteredMinVersion.shift();
-  } catch (e) {
-    console.log('Git version number is inconsistent');
-    throw e;
+export const parseVersion = (input, regex) => {
+  const matches = input.match(regex);
+  if (!matches || R.isEmpty(matches)) {
+    return BAD_VERSION;
   }
 
-  const actualVersionNormalized = generateNormalizedVersionNumber(filteredActualVersion);
-  const minVersionNormailzed = generateNormalizedVersionNumber(filteredMinVersion);
-
-  return actualVersionNormalized >= minVersionNormailzed;
+  const numericVersionNumbers = R.filter(match => !isNaN(match), matches);
+  return normalizeVersion(numericVersionNumbers);
 };
 
-const isAtleastLfsVersion = (actualVersion, minVersion) => {
-  const lfsVersionRegex = /(?:git-lfs\/\s+)?(\d+)(?:.(\d+))?(?:.(\d+))?.*/g;
+export const isAtleastGitVersion = gitInput =>
+  parseVersion(gitInput, versionRegexes.GIT) >= minimumVersions.GIT;
 
-  let filteredActualVersion;
-  let filteredMinVersion;
+export const isAtleastLfsVersion = lfsInput =>
+  parseVersion(lfsInput, versionRegexes.LFS) >= minimumVersions.LFS;
 
-  try {
-    const minVersionMatch = lfsVersionRegex.exec(minVersion);
-    const actualVersionMatch = lfsVersionRegex.exec(actualVersion);
-    filteredActualVersion = actualVersionMatch.filter(match => !isNaN(parseInt(match, 10)));
-    filteredMinVersion = minVersionMatch.filter(match => !isNaN(parseInt(match, 10)));
-    // remove the full string match
-    filteredActualVersion.shift();
-    filteredMinVersion.shift();
-  } catch (e) {
-    console.log('Git LFS version number is inconsistent');
-    throw e;
-  }
-
-  const actualVersionNormalized = generateNormalizedVersionNumber(filteredActualVersion);
-  const minVersionNormailzed = generateNormalizedVersionNumber(filteredMinVersion);
-
-  return actualVersionNormalized >= minVersionNormailzed;
-};
-
-const isLfsRepo = workingDir => fse.pathExists(path.join(workingDir), '.git', 'lfs');
-
-module.exports = {
-  isAtleastGitVersion,
-  isAtleastLfsVersion,
-  isLfsRepo,
-};
+export const isLfsRepo = workingDir => fse.pathExists(path.join(workingDir), '.git', 'lfs');
