025ff68b74c9d6755be21ddc18802ab0632264a8
Mohseen Mukaddam <mohseenmukaddam6@gmail.com>
2017-07-31 15:38:23 -0700

    Core command fixes for checkout, fetch, pull

--- src/commands/checkout.js
+++ src/commands/checkout.js
@@ -1,11 +1,47 @@
 import { core } from './lfsCommands';
-import generateResponse from '../utils/generateResponse';
 import { BAD_CORE_RESPONSE } from '../constants';
+import generateResponse from '../utils/generateResponse';
 
-const checkout = () => {
+function checkout(repo, remoteArg, branchArg) {
   //eslint-disable-next-line
   let response = generateResponse();
-  return core.checkout()
+
+  if (repo && branchArg && remoteArg) {
+    return core.checkout(`${remoteArg} ${branchArg}`, { cwd: repo.path() }).then(({ stdout, stderr }) => {
+      response.raw = stdout;
+      response.stderr = stderr;
+      return response;
+    }).catch((error) => {
+      response.success = false;
+      response.error = error.errno || BAD_CORE_RESPONSE;
+      response.raw = error;
+      return response;
+    });
+  }
+
+  let remoteRef;
+  //eslint-disable-next-line
+  let branch;
+  let remoteName;
+
+  return repo.getCurrentBranch()
+    .then((Reference) => {
+      //eslint-disable-next-line
+      let promises = [];
+      promises.push(this.NodeGit.Branch.upstream(Reference));
+      promises.push(this.NodeGit.Branch.name(Reference));
+      return Promise.all(promises);
+    })
+    .then((results) => {
+      remoteRef = results[0];
+      branch = branchArg || results[1];
+      //eslint-disable-next-line
+      return this.NodeGit.Branch.remoteName(repo, remoteRef.name());
+    })
+    .then((name) => {
+      remoteName = remoteArg || name;
+      return core.checkout(`${remoteName} ${branch}`, { cwd: repo.path() });
+    })
     .then(({ stdout, stderr }) => {
       response.raw = stdout;
       response.stderr = stderr;
@@ -17,5 +53,6 @@ const checkout = () => {
       response.raw = error;
       return response;
     });
-};
+}
+
 export default checkout;
--- src/commands/fetch.js
+++ src/commands/fetch.js
@@ -1,25 +1,58 @@
 import { core } from './lfsCommands';
+import { BAD_CORE_RESPONSE } from '../constants';
+import generateResponse from '../utils/generateResponse';
 
-const fetch = () => {
-  // get the default remote / branch
-  const remote = 'origin';
-  const branch = 'master';
-  const args = `${remote} ${branch}`;
-  return core.fetch(args);
-};
-/* {
-  success: boolean,
-  raw: '',
-  errno: '',
-  version: '1.8.0'
-}
-{
-  success: boolean,
-  raw: '',
-  errno: '',
-  push: {
-    bytes: '',
-    objects: ''
+function fetch(repo, remoteArg, branchArg) {
+  //eslint-disable-next-line
+  let response = generateResponse();
+
+  if (repo && branchArg && remoteArg) {
+    return core.fetch(`${remoteArg} ${branchArg}`, { cwd: repo.path() }).then(({ stdout, stderr }) => {
+      response.raw = stdout;
+      response.stderr = stderr;
+      return response;
+    }).catch((error) => {
+      response.success = false;
+      response.error = error.errno || BAD_CORE_RESPONSE;
+      response.raw = error;
+      return response;
+    });
   }
-} */
+
+  let remoteRef;
+  //eslint-disable-next-line
+  let branch;
+  let remoteName;
+
+  return repo.getCurrentBranch()
+    .then((Reference) => {
+      //eslint-disable-next-line
+      let promises = [];
+      promises.push(this.NodeGit.Branch.upstream(Reference));
+      promises.push(this.NodeGit.Branch.name(Reference));
+      return Promise.all(promises);
+    })
+    .then((results) => {
+      remoteRef = results[0];
+      branch = branchArg || results[1];
+      //eslint-disable-next-line
+      return this.NodeGit.Branch.remoteName(repo, remoteRef.name());
+    })
+    .then((name) => {
+      remoteName = remoteArg || name;
+      return core.fetch(`${remoteName} ${branch}`, { cwd: repo.path() });
+    })
+    .then(({ stdout, stderr }) => {
+      response.raw = stdout;
+      response.stderr = stderr;
+      return response;
+    })
+    .catch((error) => {
+      response.success = false;
+      response.error = error.errno || BAD_CORE_RESPONSE;
+      response.raw = error;
+      return response;
+    });
+}
+
 export default fetch;
