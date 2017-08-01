4013ea46d0bd0b4b9ab803aa0b82d4bdc7f95c7e
Mohseen Mukaddam <mohseenmukaddam6@gmail.com>
2017-08-01 10:31:05 -0700

    core ls command

--- src/commands/ls.js
+++ src/commands/ls.js
@@ -1,9 +1,26 @@
 import path from 'path';
+import R from 'ramda';
 import { core } from './lfsCommands';
 import {
   BAD_CORE_RESPONSE,
 } from '../constants';
 import generateResponse from '../utils/generateResponse';
+
+const isValidFileOutput = str => str.includes('*') || str.includes('-');
+
+const extractOnlyFileNames = (str) => {
+  if (str.includes('*')) { return str.split('*')[1].trim(); }
+  return str.split('-')[1].trim();
+};
+
+const extractFileNames = (raw) => {
+  if (raw && typeof raw === 'string') {
+    const outputLines = raw.split('\n');
+    const filteredLines = R.filter(isValidFileOutput, outputLines);
+    return R.map(extractOnlyFileNames, filteredLines);
+  }
+  return null;
+};
 
 const ls = (repo, args) => {
   //eslint-disable-next-line
@@ -13,8 +30,10 @@ const ls = (repo, args) => {
 
   return core.ls(args, { cwd: repoPath })
     .then(({ stdout, stderr }) => {
+      // let files = [];
       response.raw = stdout;
       response.stderr = stderr;
+      response.files = extractFileNames(stdout);
       return response;
     })
     .catch((error) => {
