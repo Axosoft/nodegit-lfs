ffaabd1028b430057b8b8d6ef0aff5096a2b00bb
Mohseen Mukaddam <mohseenmukaddam6@gmail.com>
2017-08-07 15:10:55 -0700

    adding lfs server script runner

---
+++ test/server/lfs-server.js
@@ -0,0 +1,40 @@
+#!/usr/bin/env node
+/* eslint-disable */
+const cp = require('child_process');
+
+const runScript = `
+et -eu
+set -o pipefail
+
+
+LFS_LISTEN="tcp://:9001"
+LFS_HOST="127.0.0.1:9001"
+LFS_CONTENTPATH="content"
+LFS_ADMINUSER="admin"
+LFS_ADMINPASS="admin"
+LFS_SCHEME="http"
+
+export LFS_LISTEN LFS_HOST LFS_CONTENTPATH LFS_ADMINUSER LFS_ADMINPASS LFS_SCHEME
+
+$TRAVIS_BUILD_DIR/test/server/bin/lfs-test-server
+`;
+
+const lfsConfig = `
+[lfs]
+    url = "http://127.0.0.1:9001/"
+[http]
+	sslverify = false
+`;
+
+cp.execSync(`echo "${lfsConfig}" > .lfsconfig`, { 
+  cwd: __dirname,
+  shell: true
+});
+cp.execSync(`echo "${runScript}" > run.sh && chmod +x run.sh`, { 
+  cwd: __dirname,
+  shell: true
+});
+cp.execSync('go get github.com/github/lfs-test-server', { 
+  cwd: __dirname,
+  shell: true
+});
