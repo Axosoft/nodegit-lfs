16fb732cc840e78f43038eeaf2dfe0bb6dfaa244
Mohseen Mukaddam <mohseenmukaddam6@gmail.com>
2017-08-10 13:50:32 -0700

    lfs ci server script fix

--- test/server/lfs-server.js
+++ test/server/lfs-server.js
@@ -19,13 +19,6 @@ export LFS_LISTEN LFS_HOST LFS_CONTENTPATH LFS_ADMINUSER LFS_ADMINPASS LFS_SCHEM
 $TRAVIS_BUILD_DIR/test/server/bin/lfs-test-server
 `;
 
-const lfsConfig = `
-[lfs]
-    url = "http://127.0.0.1:9001/"
-[http]
-	sslverify = false
-`;
-
 cp.execSync(`echo "${runScript}" > run.sh && chmod +x run.sh`, { 
   cwd: __dirname,
   shell: true
