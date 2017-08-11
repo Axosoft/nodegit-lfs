858efc4c25b8b9b6805aa48062d7dd27bcf5b646
Carson Howard <carsonh@axosoft.com>
2017-08-11 13:04:21 -0700

    Fixed apply for git lfs clean

--- src/callbacks/apply.js
+++ src/callbacks/apply.js
@@ -1,33 +1,14 @@
 import cp from 'child_process';
 import { Error } from 'nodegit';
-import fse from 'fs-extra';
-import path from 'path';
-import { exec } from '../utils/execHelpers';
-import { exec as spawn } from '../utils/spawnHelper';
 
 const clean = (to, from, source) => {
-  debugger;
   const workdir = source.repo().workdir();
-  const filePath = path.join(source.repo().workdir(), source.path());
-  const command = 'git lfs clean';
+  const command = `cat ${source.path()} | git lfs clean ${source.path()}`;
 
-  try {
-    const buf = fse.readFileSync(filePath);
-    // const stdout = cp.execSync(command, { env: process.env, cwd: workdir, input: buf, maxBuffer: buf.length * 2, shell: true })
-    return spawn(command, { cwd: workdir, input: buf, shell: true })
-      .then(({ stdout, stderr }) => {
-        console.log(stdout);
-        const sha = new Buffer(stdout);
-        return to.set(sha, sha.length).then(() => Error.CODE.OK);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-    // const sha = new Buffer(stdout);
-    // return to.set(sha, sha.length).then(() => Error.CODE.OK);
-  } catch (e) {
-    return Error.CODE.PASSTHROUGH;
-  }
+  // also does not work with async
+  const stdout = cp.execSync(command, { cwd: workdir });
+  const sha = new Buffer(stdout);
+  return to.set(sha, sha.length).then(() => Error.CODE.OK);
 };
 
 const smudge = (to, from, source) => {
