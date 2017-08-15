f2d00cd023732832b39dc4f275abcf5bf29ce9d7
Joshua Grosso <joshuag@axosoft.com>
2017-08-15 10:56:57 -0700

    Finish 'Apply clean' spec

--- test/tests/callbacks/apply.spec.js
+++ test/tests/callbacks/apply.spec.js
@@ -1,11 +1,18 @@
-import path from 'path';
+import {
+  expect
+} from 'chai';
+import fse from 'fs-extra';
 import NodeGit from 'nodegit';
+import path from 'path';
+
 import {
   createDummyFile,
+  getFilePointer,
   todo
 } from '../../utils';
+
 import LFS from '../../../build/src';
-import track from '../../../build/src/commands/track';
+import trackCommand from '../../../build/src/commands/track';
 
 const commitFile = (repo, fileName, commitMessage) => {
   let index;
@@ -38,21 +45,44 @@ const commitFile = (repo, fileName, commitMessage) => {
         [parent]));
 };
 
+const track = (repo, globs) => {
+  let index;
+
+  return trackCommand(repo, globs)
+    .then(() => repo.index())
+    .then((_index) => {
+      index = _index;
+      return index.addByPath('.gitattributes');
+    })
+    .then(() => index.write());
+};
+
 describe('Apply', () => {
-  it('Clean', () => {
+  it.only('Clean', () => {
     const lfsTestRepoPath = path.resolve(__dirname, '..', '..', 'repos', 'lfs-test-repository');
     const NodeGitLFS = LFS(NodeGit);
     let repository;
+    let dummyContents;
 
     return NodeGitLFS.Repository.open(lfsTestRepoPath)
       .then((repo) => {
         repository = repo;
-        return track(repo, ['*.md']);
+        return track(repo, ['big_file_test.md']);
       })
       .then(() => NodeGitLFS.LFS.register())
       .then(() => createDummyFile(path.join(lfsTestRepoPath, 'big_file_test.md'), 20))
+      .then((contents) => {
+        dummyContents = contents;
+      })
       .then(() => commitFile(repository, 'big_file_test.md', 'LFS Clean Test'))
-      .then(() => todo());
+      .then(() => fse.readFile(path.join(lfsTestRepoPath, 'big_file_test.md'), 'utf8'))
+      .then((contents) => {
+        expect(contents).to.equal(dummyContents);
+      })
+      .then(() => getFilePointer(lfsTestRepoPath, 'big_file_test.md'))
+      .then((pointer) => {
+        expect(pointer).to.have.string('size 20');
+      });
   });
 
   it('Smudge', () => {
--- test/utils.js
+++ test/utils.js
@@ -2,10 +2,13 @@ import { expect } from 'chai';
 import crypto from 'crypto';
 import fse from 'fs-extra';
 
+import spawn from '../build/src/utils/spawnHelper';
+
 export const createDummyFile = (fileName, length) => {
   // https://github.com/sindresorhus/crypto-random-string/blob/master/index.js#L9
   const contents = crypto.randomBytes(Math.ceil(length / 2)).toString('hex').slice(0, length);
-  return fse.writeFile(fileName, contents);
+  return fse.writeFile(fileName, contents)
+    .then(() => contents);
 };
 
 export const fail = (msg) => {
@@ -15,3 +18,7 @@ export const fail = (msg) => {
 export const todo = () => {
   fail('TODO');
 };
+
+export const getFilePointer = (workdir, fileName) =>
+  spawn(`git show HEAD:${fileName}`, { cwd: workdir, env: { GIT_PAGER: 'cat' } })
+    .then(({ stdout }) => stdout.toString());
