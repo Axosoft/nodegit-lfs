b60820961cad4a48d16c7f203b86c5e1073b3b49
Joshua Grosso <joshuag@axosoft.com>
2017-08-15 14:45:52 -0700

    Refactor 'Amend Check/Smudge' tests

--- test/runner.js
+++ test/runner.js
@@ -9,46 +9,51 @@ const git = require('../build/src/commands/lfsCommands').core.git;
 const testLFSServer = require('./server/server');
 
 const testReposPath = path.join(__dirname, 'repos');
-const lfsRepoPath = path.join(testReposPath, 'lfs-test-repository');
-const emptyRepoPath = path.join(testReposPath, 'empty');
-
 before(function () { // eslint-disable-line prefer-arrow-callback
   this.timeout(30000);
+
+  this.NodeGitLFS = LFS(NodeGit);
+
+  this.emptyRepoPath = path.join(testReposPath, 'empty');
+  this.lfsTestRepoPath = path.join(testReposPath, 'lfs-test-repository');
 
   const testRepoUrl = 'https://github.com/jgrosso/nodegit-lfs-test-repo';
   return testLFSServer.start()
     .then(() => fse.remove(testReposPath))
     .then(() => fse.mkdir(testReposPath))
-    .then(() => fse.mkdir(lfsRepoPath))
-    .then(() => fse.mkdir(emptyRepoPath))
-    .then(() => git(`init ${emptyRepoPath}`))
-    .then(() => git(`clone ${testRepoUrl} ${lfsRepoPath}`, {
+    .then(() => fse.mkdir(this.lfsTestRepoPath))
+    .then(() => fse.mkdir(this.emptyRepoPath))
+    .then(() => git(`init ${this.emptyRepoPath}`))
+    .then(() => git(`clone ${testRepoUrl} ${this.lfsTestRepoPath}`, {
       env: {
         GIT_SSL_NO_VERIFY: 1
       }
     }))
     .then(() => fse.appendFile(
-      path.join(lfsRepoPath, '.git', 'config'),
+      path.join(this.lfsTestRepoPath, '.git', 'config'),
 `[http]
   sslverify = false`
     ))
-    .catch((err) => { throw new Error(err); });
+    .catch((err) => {
+      throw new Error(err);
+    });
 });
 
-beforeEach(() => {
-  return exec('git clean -xdf && git reset --hard', { cwd: lfsRepoPath })
-    .then(() => exec('git clean -xdff', { cwd: emptyRepoPath }));
+beforeEach(function () {
+  return exec('git clean -xdf && git reset --hard', { cwd: this.lfsTestRepoPath })
+    .then(() => exec('git clean -xdff', { cwd: this.emptyRepoPath }));
 });
 
 after(() => {
   testLFSServer.stop();
 });
 
-afterEach(() => {
-  const NodeGitLFS = LFS(NodeGit);
-  return NodeGitLFS.LFS.unregister()
+afterEach(function () {
+  return this.NodeGitLFS.LFS.unregister()
     .catch((error) => {
       // -3 implies LFS filter was not registered
-      if (error.errno !== -3) { throw error; }
+      if (error.errno !== -3) {
+        throw error;
+      }
     });
 });
