fb9912c90a98016cac32614692cde2d66cc63957
Carson Howard <cjhoward92@gmail.com>
2017-08-30 10:15:33 -0700

    updated execHelper to always have /usr/local/bin on path with exec

--- src/utils/execHelper.js
+++ src/utils/execHelper.js
@@ -1,8 +1,15 @@
 import child from 'child_process';
+import R from 'ramda';
 
 const exec = (command, input, opts) => new Promise(
   (resolve, reject) => {
-    const proc = child.exec(command, opts, (err, stdout, stderr) => {
+    const options = R.mergeDeepRight({ env: process.env }, opts);
+
+    if (process.platform === 'darwin' && !R.contains('/usr/local/bin', options.env.PATH)) {
+      options.env.PATH = `${options.env.PATH}${':/usr/local/bin'}`;
+    }
+
+    const proc = child.exec(command, options, (err, stdout, stderr) => {
       if (err) {
         reject(err);
       } else {
