81521018fd049872303d55d42d2904f71f0e2b39
Joshua Grosso <joshuag@axosoft.com>
2017-09-05 14:39:28 -0700

    Add `errorCatchHandler` spec stubs

--- test/tests/helpers.spec.js
+++ test/tests/helpers.spec.js
@@ -1,6 +1,7 @@
 import {
   expect
 } from 'chai';
+import fse from 'fs-extra';
 import path from 'path';
 
 import {
@@ -8,17 +9,32 @@ import {
   todo
 } from '../utils';
 
-import * as helpers from '../../build/src/helpers';
+import {
+  getGitattributesPathFromRepo,
+  loadGitattributeFiltersFromRepo,
+  repoHasLfsFilters,
+  repoHasLfsObjectBin
+} from '../../build/src/helpers';
 import track from '../../build/src/commands/track';
 
 describe('helpers', () => {
+  describe.only('errorCatchHandler', () => {
+    describe('when the error is from LFS', () => {
+      it('returns a response object', todo);
+    });
+
+    describe('when the error is not from LFS', () => {
+      it('rethrows the error code', todo);
+    });
+  });
+
   describe('getGitattributesPathFromRepo', () => {
     it("gets the location of a repository's `.gitattributes` file", function () {
       const {
         lfsTestRepo
       } = this;
 
-      expect(helpers.getGitattributesPathFromRepo(lfsTestRepo))
+      expect(getGitattributesPathFromRepo(lfsTestRepo))
         .to.equal(path.join(lfsTestRepo.workdir(), '.gitattributes'));
     });
   });
@@ -30,7 +46,7 @@ describe('helpers', () => {
       } = this;
 
       return track(lfsTestRepo, ['*.md', 'test.txt'])
-        .then(() => helpers.loadGitattributeFiltersFromRepo(lfsTestRepo))
+        .then(() => loadGitattributeFiltersFromRepo(lfsTestRepo))
         .then((result) => {
           expect(result).to.have.members(['*.md', 'test.txt']);
         });
@@ -42,32 +58,12 @@ describe('helpers', () => {
           emptyRepo
         } = this;
 
-        return helpers.loadGitattributeFiltersFromRepo(emptyRepo)
+        return loadGitattributeFiltersFromRepo(emptyRepo)
           .then(() => fail('Expected promise to fail!'))
           .catch((err) => {
             expect(err.message).to.equal('No .gitattributes found');
           });
       });
-    });
-  });
-
-  describe('repoHasLfsFilters', () => {
-    describe('when the provided repository has `.gitattributes` filters', () => {
-      it('returns `true`', todo);
-    });
-
-    describe('when the provided repository has no `.gitattributes` filters', () => {
-      it('returns `false`', todo);
-    });
-  });
-
-  describe('repoHasLfsObjectBin', () => {
-    describe('when `.git/lfs` exists in the provided repository', () => {
-      it('returns `true`', todo);
-    });
-
-    describe('when `.git/lfs` does not exist in the provided repository', () => {
-      it('returns `false`', todo);
     });
   });
 
@@ -81,13 +77,65 @@ describe('helpers', () => {
     });
   });
 
-  describe('errorCatchHandler', () => {
-    describe('when the error is from LFS', () => {
-      it('returns a response object', todo);
+  describe('repoHasLfsFilters', () => {
+    describe('when the provided repository has `.gitattributes` filters', () => {
+      it('returns `true`', function () {
+        const {
+          lfsTestRepo
+        } = this;
+
+        return track(lfsTestRepo, ['*.md'])
+          .then(() => repoHasLfsFilters(lfsTestRepo))
+          .then((result) => {
+            expect(result).to.be.true;
+          });
+      });
     });
 
-    describe('when the error is not from LFS', () => {
-      it('rethrows the error code', todo);
+    describe('when the provided repository has no `.gitattributes` filters', () => {
+      it('returns `false`', function () {
+        const {
+          lfsTestRepo
+        } = this;
+
+        return repoHasLfsFilters(lfsTestRepo)
+          .then((result) => {
+            expect(result).to.be.false;
+          });
+      });
     });
+  });
+
+  describe('repoHasLfsObjectBin', () => {
+    describe('when `.git/lfs` exists in the provided repository', () => {
+      it('returns `true`', function () {
+        const {
+          lfsTestRepo
+        } = this;
+
+        return fse.mkdir(path.join(lfsTestRepo.path(), 'lfs'))
+          .then(() => repoHasLfsObjectBin(lfsTestRepo))
+          .then((result) => {
+            expect(result).to.be.true;
+          });
+      });
+    });
+
+    describe('when `.git/lfs` does not exist in the provided repository', () => {
+      it('returns `false`', function () {
+        const {
+          emptyRepo
+        } = this;
+
+        return repoHasLfsObjectBin(emptyRepo)
+          .then((result) => {
+            expect(result).to.be.false;
+          });
+      });
+    });
+  });
+
+  describe('verifyOutput', () => {
+    describe('when the provided stats ')
   });
 });
