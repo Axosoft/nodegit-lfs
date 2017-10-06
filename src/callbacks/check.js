6832d709c4155c8a49a3c9c734f6254cd962217e
Carson Howard <carsonh+1@axosoft.com>
2017-10-06 08:24:42 -0700

    Linter

--- src/callbacks/check.js
+++ src/callbacks/check.js
@@ -1,4 +1,4 @@
 import R from 'ramda';
 import { Error } from '../constants';
 
-export default (src, attr) => attr && R.equals('lfs', attr.toLowerCase()) ? Error.CODE.OK : Error.CODE.PASSTHROUGH;
+export default (src, attr) => (attr && R.equals('lfs', attr.toLowerCase()) ? Error.CODE.OK : Error.CODE.PASSTHROUGH);
