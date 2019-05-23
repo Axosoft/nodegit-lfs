e0b5233168817b6ee23f842dce9ff58f2e355027
Steven King Jr <stevek@axosoft.com>
2019-05-23 12:01:30 -0700

    Update call sites for spawn

--- src/callbacks/apply.js
+++ src/callbacks/apply.js
@@ -1,16 +1,11 @@
 import fse from 'fs-extra';
 import path from 'path';
 import { Error } from '../constants';
-import { spawnShell, winSpawn } from '../utils/spawnHelper';
+import spawn from '../utils/spawnHelper';
 import exec from '../utils/execHelper';
 
 const IS_WINDOWS = process.platform === 'win32';
 const ticks = IS_WINDOWS ? '"' : '\'';
-
-const parseSize = (ptr) => {
-  const idx = ptr.indexOf('size ') + 5;
-  return Number(ptr.substring(idx).trim());
-};
 
 export default (credentialsCallback) => {
   const clean = (to, from, source) => source.repo()
@@ -34,24 +29,19 @@ export default (credentialsCallback) => {
       const parts = source.path().split('/');
       const filepath = parts[parts.length - 1];
       const ptr = from.ptr();
-      const size = parseSize(ptr);
-      const echo = IS_WINDOWS ? `echo|set /p="${ptr}"` : `echo -ne "${ptr}"`;
 
-      const promise = IS_WINDOWS
-        ? winSpawn(`git lfs smudge ${ticks}${filepath}${ticks}`, ptr, { cwd: workdir })
-        : spawnShell(
-          `${echo} | git lfs smudge ${ticks}${filepath}${ticks}`,
-          { cwd: workdir },
-          size,
-          credentialsCallback
-        );
+      const promise = spawn(
+        `git lfs smudge ${ticks}${filepath}${ticks}`,
+        { cwd: workdir },
+        workdir,
+        credentialsCallback,
+        ptr
+      );
 
       return promise;
     })
     .then(({ stdout }) => to.set(stdout, stdout.length))
     .then(() => Error.CODE.OK);
-
-  let previousFilterPromise = Promise.resolve();
 
   return (to, from, source) => {
     const mode = source.mode();
@@ -68,9 +58,6 @@ export default (credentialsCallback) => {
         () => Error.CODE.PASSTHROUGH
       );
 
-    previousFilterPromise = previousFilterPromise
-      .then(runNextFilter, runNextFilter);
-
-    return previousFilterPromise;
+    return runNextFilter();
   };
 };
--- src/commands/lfsCommands.js
+++ src/commands/lfsCommands.js
@@ -2,9 +2,9 @@ import spawn from '../utils/spawnHelper';
 import exec from '../utils/execHelper';
 
 export const core = {
-  checkout: (args = '', options, callback) => spawn(`git lfs checkout ${args}`, options, callback),
-  clone: (args = '', options, callback) => spawn(`git lfs clone ${args}`, options, callback),
-  fetch: (args = '', options, callback) => spawn(`git lfs fetch ${args}`, options, callback),
+  checkout: (args = '', options, callback) => spawn(`git lfs checkout ${args}`, options, '', callback),
+  clone: (args = '', options, callback) => spawn(`git lfs clone ${args}`, options, '', callback),
+  fetch: (args = '', options, callback) => spawn(`git lfs fetch ${args}`, options, '', callback),
   fsck: options => exec('git lfs fsck', null, options),
   git: (args = '', options) => exec(`git ${args}`, null, options),
   install: (args = '', options) => exec(`git lfs install ${args}`, null, options),
@@ -12,8 +12,8 @@ export const core = {
   ls: (args = '', options) => exec(`git lfs ls-files ${args}`, null, options),
   pointer: (args = '', options) => exec(`git lfs pointer ${args}`, null, options),
   prune: (args = '', options) => exec(`git lfs prune ${args}`, null, options),
-  pull: (args = '', options, callback) => spawn(`git lfs pull ${args}`, options, callback),
-  push: (args = '', options, callback) => spawn(`git lfs push ${args}`, options, callback),
+  pull: (args = '', options, callback) => spawn(`git lfs pull ${args}`, options, '', callback),
+  push: (args = '', options, callback) => spawn(`git lfs push ${args}`, options, '', callback),
   status: (args = '', options) => exec(`git lfs status ${args}`, null, options),
   track: (args = '', options) => exec(`git lfs track ${args}`, null, options),
   untrack: (args = '', options) => exec(`git lfs untrack ${args}`, null, options),
